6a0dc07de7ac0288ea7310a8a143965a5ce47bbc
Phil-Fan <hw.phil.fan@gmail.com>
2026-05-28 11:10:24 +0800

    feat: add allowedDevOrigins for local development in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const fetchInterval = process.env.FETCH_INTERVAL;
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["192.168.*.*","localhost","127.0.0.1"],
   reactCompiler: true,
   env: {
     ...(fetchInterval ? { NEXT_PUBLIC_FETCH_INTERVAL: fetchInterval } : {}),
